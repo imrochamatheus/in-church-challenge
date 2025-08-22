5a78e76a401106094e4fd58188942ba1b55f0beb
Matheus Rocha <im.rochamatheus@gmail.com>
2025-08-22 20:22:40 -0300

    chore(setup): estrutura inicial da aplicação

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { authRoutes } from './features/auth/auth.routes';
+import { authGuard } from './core/guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/auth/entrar',
+  },
+  {
+    path: 'auth',
+    children: [...authRoutes],
+  },
+  {
+    path: '**',
+    redirectTo: '/auth/entrar',
+  },
+];
