1f624a9ad1852de4fcbc7e5ddf3cefe051806048
Matheus Rocha <im.rochamatheus@gmail.com>
2025-08-23 00:38:28 -0300

    refactor(routes): adicionar rota para página de eventos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,23 @@
 import { Routes } from '@angular/router';
 import { authRoutes } from './features/auth/auth.routes';
+import { MainLayoutComponent } from './core/layouts/main-layout/main-layout.component';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/auth/entrar',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        title: 'Eventos',
+        canMatch: [authGuard],
+        loadComponent: () =>
+          import(
+            './features/events/pages/events-page/events-page.component'
+          ).then((m) => m.EventsPageComponent),
+      },
+    ],
   },
   {
     path: 'auth',
