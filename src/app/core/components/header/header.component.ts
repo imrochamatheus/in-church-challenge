47a3754a868693624889bb2a1eed8437f4e8a87a
Matheus Rocha <im.rochamatheus@gmail.com>
2025-08-22 23:58:50 -0300

    feat(header): adiciona componente de header

---
+++ src/app/core/components/header/header.component.ts
@@ -0,0 +1,40 @@
+import { Component, input, output, signal } from '@angular/core';
+import {
+  Grid3x3,
+  Search,
+  Funnel,
+  Download,
+  CalendarDays,
+  LucideAngularModule,
+} from 'lucide-angular';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  imports: [LucideAngularModule],
+})
+export class HeaderComponent {
+  public searchTerm = input<string>('');
+  public search = output<string>();
+  public newEvent = output<void>();
+
+  public isSearchOpen = signal(false);
+
+  public readonly icons = {
+    grid: Grid3x3,
+    search: Search,
+    funnel: Funnel,
+    download: Download,
+    calendarDays: CalendarDays,
+  };
+
+  public onInput(e: Event): void {
+    const term = (e.target as HTMLInputElement).value;
+    this.search.emit(term);
+  }
+
+  public toggleSearch() {
+    this.isSearchOpen.update((value) => !value);
+  }
+}
